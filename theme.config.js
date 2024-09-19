5b32e70eb757dcf0b7fb5edc4670daff6c2b89bb
myschproj <150742169+myschproj@users.noreply.github.com>
2024-09-19 23:50:03 +0300

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -25,19 +25,19 @@ export default {
   footer: (
     <>
       <small style={{ display: 'block', marginTop: '8rem' }}>
-        <time>{YEAR}</time> © Mohsin Hayat.
+        <time>{YEAR}</time> with love by adgb.ru.
         <a className="icon-link" href="/feed.xml">
           <i className="fa fa-solid fa-rss"></i>
         </a>
-        <a className="icon-link" href="https://facebook.com/mohsinhayatt">
+        <a className="icon-link" href="https://facebook.com/sarmatello">
           <i className="fa fa-brands fa-facebook"></i>
         </a>
-        <a className="icon-link" href="https://github.com/mohsinht">
+        <a className="icon-link" href="https://github.com/fallenmor">
           <i className="fa fa-brands fa-github"></i>
         </a>
         <a
           className="icon-link"
-          href="https://www.linkedin.com/in/mohsinhayatt/"
+          href="https://www.linkedin.com/in/sarmatello"
         >
           <i className="fa fa-brands fa-linkedin"></i>
         </a>
@@ -59,9 +59,9 @@ export default {
   logo: (
     <>
       <svg>...</svg>
-      <span>Mohsin Hayat</span>
+      <span>Сармат Гаппоев</span>
     </>
   ),
-  titleSuffix: " – Mohsin Hayat's Resumé",
+  titleSuffix: " – Сайт Гаппоева Сармата",
   back: 'Read More →',
 }
